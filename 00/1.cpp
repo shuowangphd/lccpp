578db6de9539abb820979ecd95a694c93541da2e
shuowangphd <shuooo@gmail.com>
2022-03-13 17:55:46 -0600

    commit

---
+++ 00/1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> um;
+        for (int i = 0; i < nums.size(); i++) {
+            if (um.find(target - nums[i]) != um.end()) {
+                return vector<int> {um[target - nums[i]], i};
+            }
+        um[nums[i]] = i;
+        }
+        return {};
+    }
+};
